12b3a7b7c9bda140c9892c4555b41980000a8794
Yosef <shmuli5770@gmail.com>
2021-04-28 12:05:47 -0400

    commit tree f1741b62791352f9cfca4afa4ccc514426830d20

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+class Polygon {
+    constructor(numArray) {
+        this.numArray = numArray
+    }
+
+    get countSides() {
+        return this.numArray.length
+    }
+
+    get perimeter() {
+        return this.numArray.reduce((a,b) => a + b)
+    }
+}
+
+class Triangle extends Polygon {
+
+    get isValid() {
+        let validator = (num1, num2, num3) => {
+            return num1+num2 > num3
+        }
+        return (
+            validator(this.numArray[0], this.numArray[1], this.numArray[2]) && 
+            validator(this.numArray[1], this.numArray[2], this.numArray[0]) && 
+            validator(this.numArray[2], this.numArray[0], this.numArray[1]) && 
+            validator(this.numArray[1], this.numArray[0], this.numArray[2]) &&
+            validator(this.numArray[2], this.numArray[1], this.numArray[0]) &&
+            validator(this.numArray[0], this.numArray[2], this.numArray[1])
+            )
+    }
+}
+
+class Square extends Polygon {
+
+    get isValid() {
+        return (
+            this.numArray[0] === this.numArray[1] &&
+            this.numArray[1] === this.numArray[2] &&
+            this.numArray[2] === this.numArray[3]
+        )
+    }
+
+    get area() {
+        return this.numArray[0] * this.numArray[1]
+    }
+}
